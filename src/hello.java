a94910835a04ace375a5bd4ed0a9b1b20de819f8
alphg <alphg@192.168.1.45>
2021-08-29 16:41:28 -0500

    Added collab name/major

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,12 @@
+
+
 public class hello {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		
 		System.out.println("Caden Chavez");
 		System.out.println("Computer Science");
+		System.out.println("Alex Gilson");
+		System.out.println("Computer Science");
+		
 	}
 }
